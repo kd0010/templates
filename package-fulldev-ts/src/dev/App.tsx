d3a6f88b382f2851e5cdbc009287fc8e1d32a173
David Kander <kanders.davids@gmail.com>
2023-10-24 15:51:28 +0300

    add template `package-fulldev-ts/`

---
+++ package-fulldev-ts/src/dev/App.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent, render } from 'preact'
+
+interface Props {
+  
+}
+
+export const App: FunctionComponent<Props> = ({
+  
+}) => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+render(
+  <App />,
+  document.getElementById('root')!,
+)
